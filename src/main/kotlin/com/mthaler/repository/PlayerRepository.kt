b84b98da7eedcfef60d287b2cd428886226d40b6
Michael Thaler <michael.thaler@dlr.de>
2023-01-30 07:54:22 +0100

    Added PlayerRepository interface.

---
+++ src/main/kotlin/com/mthaler/repository/PlayerRepository.kt
@@ -0,0 +1,14 @@
+package com.mthaler.repository
+
+import com.mthaler.model.Player
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+/**
+ * @author  Manuel Ernesto (manuelernest0)
+ * @date  05/07/22 8:45 PM
+ * @version 1.0
+ */
+
+@Repository
+interface PlayerRepository : JpaRepository<Player, Long>
