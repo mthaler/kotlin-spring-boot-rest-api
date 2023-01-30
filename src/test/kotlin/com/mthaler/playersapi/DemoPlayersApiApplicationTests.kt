b79f09d4eec598a2c0bdce99515d00c852461530
Michael Thaler <michael.thaler@dlr.de>
2023-01-30 08:38:43 +0100

    Added DemoPlayersApiApplicationTests class.

---
+++ src/test/kotlin/com/mthaler/playersapi/DemoPlayersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mthaler.playersapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoPlayersApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
