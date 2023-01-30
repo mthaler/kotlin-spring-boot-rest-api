0d2a1911fbcf27a518beafe8fd5a2c02a8aab7d1
Michael Thaler <michael.thaler@dlr.de>
2023-01-30 07:46:38 +0100

    Added some files.

---
+++ src/main/kotlin/com/mthaler/PlayersApiApplication.kt
@@ -0,0 +1,11 @@
+package com.mthaler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlayersApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlayersApiApplication>(*args)
+}
